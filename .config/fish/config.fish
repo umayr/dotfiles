0e874f47c5a37f2d3ddf434854c6331a8ea6e398
Umayr Shahid <umayr.shahid@gmail.com>
2020-02-29 21:01:13 -0500

    Add fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,69 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH /home/umayr/.local/share/omf
+# Go configurations
+set -x GOPATH $HOME/Code/go
+set -x GOBIN $GOPATH/bin
+set -x PATH $PATH $GOBIN
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /home/umayr/.config/omf
+# Rust configurations
+set -x PATH $PATH $HOME/.cargo/bin
+set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
 
-# Telmate Environment Variables
-set -gx JAVA_OPTS "-Djsse.enableSNIExtension=false"
+# PyENV configurations
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PATH $PYENV_ROOT/bin
+# pyenv-virtualenv configurations (https://github.com/pyenv/pyenv-virtualenv)
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-rvm default
+# N configurations
+set -x N_PREFIX $HOME/n
+set -x PATH $PATH $N_PREFIX/bin
 
-# Path to global npm packages.
-set -gx NPM_PATH $HOME/.npm/bin
+set -x PATH $PATH /usr/local/opt/openssl/bin
 
-# Append NPM_PATH to PATH
-set -gx PATH $PATH $NPM_PATH
+# Because of ads in npm post-install script
+set -x ADBLOCK true
 
-# Variables for golang
-set -gx GOROOT /usr/lib/go
-set -gx GOPATH $HOME/.go
-set -gx GO15VENDOREXPERIMENT 1 # Turn on go 1.5 vendor experiment
+# fzf configurations
+set -x FZF_DEFAULT_OPTS '--color fg:#F8F8F2,hl:#50FA7B,hl+:#FF79C6,bg+:#282A36,info:#BD93F9,prompt:#50FA7B,spinner:#FF79C6,pointer:#FF79C6,marker:#FF79C6'
+set -x FZF_DEFAULT_COMMAND 'fd --type file'
 
-# Append GOPATH to PATH
-set -gx PATH $PATH $GOPATH/bin
+# brew installs executable in /usr/local/sbin
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
-if [ $COLORTERM = "gnome-terminal"]
-    set -gx TERM xterm-256color
-end
+# pip3 binaries path
+set -x PATH $PATH $HOME/Library/Python/3.7/bin/
 
-source ~/.config/fish/aliases.fish
+# User Keybindings
+bind \ch "eval (history | fzf)"
+
+# Fish Dracula theme colours
+set -x COMMENT 6272a4
+set -x CYAN 8be9fd
+set -x GREEN 50fa7b
+set -x ORANGE ffb86c
+set -x PINK ff79c6
+set -x PURPLE bd93f9
+set -x RED ff5555
+set -x YELLOW f1fa8c
+
+# Fish Dracula Theme Setup
+set -U fish_color_autosuggestion $COMMENT
+set -U fish_color_command $PINK
+set -U fish_color_comment $COMMENT
+set -U fish_color_cwd $CYAN
+set -U fish_color_cwd_root $RED
+set -U fish_color_end normal
+set -U fish_color_error $RED
+set -U fish_color_escape $CYAN
+set -U fish_color_history_current $CYAN
+set -U fish_color_host normal
+set -U fish_color_match $GREEN
+set -U fish_color_normal normal
+set -U fish_color_operator $CYAN
+set -U fish_color_param normal
+set -U fish_color_quote $YELLOW
+set -U fish_color_redirection $PURPLE
+set -U fish_color_search_match \x2d\x2dbackground\x3d$PURPLE
+set -U fish_color_selection \x2d\x2dbackground\x3d$PURPLE
+set -U fish_color_user $PURPLE
+set -U fish_color_valid_path \x2d\x2dunderline
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,1 +1,55 @@
-/home/umayr/.local/share/omf/themes/sushi/fish_prompt.fish
+function fish_prompt
+	set -l symbol "λ "
+	set -l code $status
+
+	if git::is_repo
+		set -l branch (git::branch_name ^/dev/null)
+		set -l ref (git show-ref --head --abbrev | awk '{print substr($0,0,7)}' | sed -n 1p)
+		
+		if git::is_stashed
+			echo -n -s (white)"^"(off)
+		end
+
+		echo -n -s (purple)"("(off)
+		
+		if git::is_dirty
+			printf (white)"*"(off)
+		end
+
+		if command git symbolic-ref HEAD > /dev/null ^/dev/null
+			if git::is_staged
+				printf (green)"$branch"(off)
+			else
+				printf (cyan)"$branch"(off)
+			end
+		else
+			printf (dim)"$ref"(off)
+		end
+
+		for remote in (git remote)
+			set -l behind_count (echo (command git rev-list $branch..$remote/$branch ^/dev/null | wc -l | tr -d " "))
+			set -l ahead_count (echo (command git rev-list $remote/$branch..$branch ^/dev/null | wc -l | tr -d " "))
+
+			if test $ahead_count -ne 0; or test $behind_count -ne 0; and test (git remote | wc -l) -gt 1
+				echo -n -s " "(dark_purple)$remote(off)
+			end
+
+			if test $ahead_count -ne 0
+				echo -n -s (white)" +"$ahead_count(off)
+			end
+
+			if test $behind_count -ne 0
+				echo -n -s (white)" -"$behind_count(off)
+			end
+		end
+
+		echo -n -s (purple)") "(off)
+	end
+
+	if test "$code" = 0 
+		echo -n -s (purple)"$symbol"(off)
+	else
+		echo -n -s (dim)"$symbol"(off)
+	end	
+
+end
